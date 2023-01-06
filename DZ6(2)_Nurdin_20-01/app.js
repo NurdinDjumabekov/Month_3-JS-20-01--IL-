4751e8b00427134b0a1ff74f0231dad4e615eab0
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-01-06 23:17:23 +0600

    Add files via upload

--- DZ6(2)_Nurdin_20-01/app.js
+++ DZ6(2)_Nurdin_20-01/app.js
@@ -1,27 +1,29 @@
 const som = document.querySelector("#som");
 const usd = document.querySelector("#usd");
 const eu = document.querySelector("#eu");
-const convert = (elem, target, target2) => {
-  elem.oninput = () => {
-    const request = new XMLHttpRequest();
-    request.open("GET", "data.json");
-    request.setRequestHeader("Content-type", "application/json");
-    request.send();
-    request.onload = () => {
-      const data = JSON.parse(request.response);
-      if (elem === som) {
-        target.value = (elem.value / data.usd).toFixed(2);
-        target2.value = (elem.value / data.eu).toFixed(2);
-      } else if (elem === usd) {
-        target.value = (elem.value * data.usd).toFixed(2);
-        target2.value = (target.value / data.eu).toFixed(2);
-      } else if (elem === eu) {
-        target.value = (elem.value * data.eu).toFixed(2);
-        target2.value = (target.value / data.usd).toFixed(2);
-      }
-      elem.value === "" && (target.value = "");
-      elem.value === "" && (target2.value = "");
-    };
+const convert = (a, target, target2) => {
+  a.oninput = () => {
+    // const request = new XMLHttpRequest()
+    // request.open('GET', "data.json")
+    // request.setRequestHeader('Content-type', 'application/json')
+    // request.send()
+
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((json) => {
+        if (a === som) {
+          target.value = (a.value / json.usd).toFixed(2);
+          target2.value = (a.value / json.eu).toFixed(2);
+        } else if (a === usd) {
+          target.value = (a.value * json.usd).toFixed(2);
+          target2.value = (target.value / json.eu).toFixed(2);
+        } else if (a === eu) {
+          target.value = (a.value * json.eu).toFixed(2);
+          target2.value = (target.value / json.usd).toFixed(2);
+        }
+        a.value === "" && (target.value = "");
+        a.value === "" && (target2.value = "");
+      });
   };
 };
 convert(som, usd, eu);
