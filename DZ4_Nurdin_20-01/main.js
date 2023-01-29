6fe706b0972fd581262c763c98a49852534c8771
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-01-29 22:23:30 +0600

    Add files via upload

--- DZ4_Nurdin_20-01/main.js
+++ DZ4_Nurdin_20-01/main.js
@@ -1,11 +1,11 @@
-const jsonInfo = () => {
-  const info = new XMLHttpRequest(); // СОЗДАНИЕ ЗАПРОСА
-  info.open("GET", "data.json"); // обьявление метода и пути запроса
-  info.setRequestHeader("Content-type", "application/json"); // указание типа данных в запросах
-  info.send(); //отправка запроса
-  info.addEventListener("load", () => {
-    const objJsonDate = JSON.parse(info.response);
-    console.log(objJsonDate);
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const request = new XMLHttpRequest(); // СОЗДАНИЕ ЗАПРОСА
+  request.open("GET", "data.json"); // обьявление метода и пути запроса
+  request.setRequestHeader("Content-type", "application/json"); // указание типа данных в запросах
+  request.send(); //отправка запроса
+  request.addEventListener("load", () => {
+    const info = JSON.parse(request.response);
+    console.log(info);
   });
-};
-jsonInfo();
+});
